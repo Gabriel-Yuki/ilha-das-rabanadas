f2af6f8b94a6149078ba999cfb3f0e3dbd22b8f9
Jonathan  Guimarães <guimaraesjonathan92@gmail.com>
2023-02-07 11:50:35 -0300

    terminei a pagina de login
    fazer a pagina de atualizar produto

--- app/Views/Produto/CadastrarProduto.php
+++ app/Views/Produto/CadastrarProduto.php
@@ -1,5 +1,3 @@
-
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -8,41 +6,61 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+
+    <style>
+        #container-form {
+            background-color: rgba(217, 180, 72, 0.3);
+        }
+
+        form {
+            max-width: 80%;
+        }
+    </style>
 </head>
 
 <body>
+    <main>
+        <div id="container-form">
 
-    <form action="insert" method="POST" enctype="multipart/form-data">
-        <label for="titulo">
-            <p>Nome</p>
-            <input type="text" name="nome" id="nome">
+            <form action="insert" method="POST" enctype="multipart/form-data" class="d-flex flex-column">
+                <label for="nome">
+                    <p>Nome</p>
+                    <input type="text" name="nome" id="nome">
 
-        </label>
-        <label for="descricao">
-            <p>Descrição</p>
+                </label>
+                <label for="descricao">
+                    <p>Descrição</p>
 
-            <input type="text" name="descricao" id="descricao">
+                    <input type="text" name="descricao" id="descricao">
 
-        </label>
-        <label for="descricao">
-            <p>Categoria</p>
+                </label>
+                <label for="descricao">
+                    <p>Categoria</p>
 
-            <input type="text" name="tipo" id="tipo">
+                    <input type="text" name="tipo" id="tipo">
 
-        </label>
-        <label for="descricao">
-            <p>Valor</p>
+                </label>
+                <label for="descricao">
+                    <p>Valor</p>
 
-            <input type="text" name="preco" id="preco">
+                    <input type="text" name="preco" id="preco">
 
-        </label>
-        <label for="imagemProduto">
-            
-    <p>Faça um upload de uma imagem</p>
-        <input type="file" name='arquivo' multiple>
-        </label>
-        <input type="submit" value="cadastrar">
-    </form>
+                </label>
+                <label for="imagemProduto">
+
+                    <p>Faça um upload de uma imagem</p>
+                    <input type="file" name='arquivo' multiple>
+                </label>
+                <input type="submit" value="cadastrar">
+            </form>
+        </div>
+    </main>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
+    </script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js" integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous">
+    </script>
 </body>
 
 </html>
--- app/Views/login/login.php
+++ app/Views/login/login.php
@@ -1,14 +1,80 @@
-<form method="POST" >
-  <div class="mb-3">
-    <label for="email" class="form-label">Email</label>
-    <input type="email" class="form-control" name="email" id="email" placeholder="abc@mail.com">
-  </div>
-  <div class="mb-3">
-    <label for="" class="form-label">Password</label>
-    <input type="password" class="form-control" name="password" id="password" placeholder="password">
-  </div>
-  <p>Esqueceu a senha?</p>
-  <a href="./index"><input class="btn btn-success" type="submit" value="Entrar" name="SendLogin"></a>
+<!doctype html>
+<html lang="en">
+
+<head>
+  <title>Ilhas das rabanadas</title>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS v5.2.1 -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+<style>
+#container-form{
+  
+  max-width: 780px;
+background-color: rgba(217, 180, 72, 0.3);
+}
+form{
+width: 50%
+
  
-  <a href="./cadastrar"><button>Cadastrar</button></a>
-</form> 
+}
+.inputs {
+max-width: 80%
+}
+.btn{
+  width: 155px;
+  height: 40px;
+}
+#entrar{
+ background-color: rgba(65, 191, 97, 0.55)
+}
+#cadastrar{
+  background-color: rgba(255, 189, 89, 0.74);
+}
+
+
+
+</style>
+</head>
+
+<body>
+  
+  <main>
+  <div id="container-form" class="m-auto d-flex flex-column align-items-center">
+    <div class="header text-center ">
+    <h1>Ilha das rabanadas</h1>
+    </div>
+    <form method="POST" >
+    <div class="inputs mb-3">
+      <label for="email" class="form-label">Email</label>
+      <input type="email" class="form-control" name="email" id="email" placeholder="abc@mail.com">
+    </div>
+    <div class="inputs mb-3">
+      <label for="" class="form-label">Password</label>
+      <input type="password" class="form-control" name="password" id="password" placeholder="password">
+    </div>
+    <p>Esqueceu a senha?</p>
+    <div class="d-flex align-items gap-3">
+     <input id="entrar" class="btn " type="submit" value="Entrar" name="SendLogin">
+      <button id="cadastrar"class="btn">Cadastrar</button>
+      
+    </div></form>
+    <span>Retornar ao site</span>
+  </div>
+
+  </main>
+  
+  <!-- Bootstrap JavaScript Libraries -->
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
+    integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
+  </script>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js"
+    integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous">
+  </script>
+</body>
+
+</html>

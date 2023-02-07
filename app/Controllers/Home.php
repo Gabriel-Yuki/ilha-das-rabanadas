6959f7c4251d81c48449cbf242a3e97871652056
Jonathan  Guimarães <guimaraesjonathan92@gmail.com>
2023-02-07 09:09:30 -0300

    subindo os diretorios
    subindo

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -2,12 +2,14 @@
 
 namespace App\Controllers;
 use Core\ConfigHome;
+use Core\ConfigView;
 
 class Home
 {
+    private $dados;
     public function index()
     {
-        $carregarhome= new ConfigHome("Views/home/home");
+        $carregarhome= new ConfigView("Home/home",$this->dados);
         $carregarhome->renderizar();
     }
 }

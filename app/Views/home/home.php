13262a665bfab4146399ab844b33bef1fbdaff92
AllanQrz <116477264+AllanQrz@users.noreply.github.com>
2023-02-09 11:46:43 -0300

    home terminar  de fazer

--- app/Views/home/home.php
+++ app/Views/home/home.php
@@ -13,7 +13,40 @@ include "./app/config.php"; ?>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
 
     <link rel="stylesheet" href="<?php echo CSS_PADRAO; ?>">
+<<<<<<< Updated upstream
 
+=======
+    <style>
+      
+
+        .banner {
+            background-image: url("<?php echo IMG ?>/img/banner.webp");
+            background-repeat: no-repeat;
+            background-size: cover;
+            background-position: center;
+            overflow: hidden;
+            height: 500px;
+        }
+
+        h1 {
+            text-align: center;
+
+        }
+
+        h1>p {
+            font-size: 25px;
+        cursor:;
+        }
+
+        .col-4 {
+            width: 20% !important;
+        }
+
+        #x {
+            background-color: rgba(217, 180, 72, 0.3);
+        }
+    </style>
+>>>>>>> Stashed changes
 </head>
 
 <body>
@@ -57,9 +90,83 @@ include "./app/config.php"; ?>
    </nav>
 
 
+<<<<<<< Updated upstream
 
 
 
+=======
+    <section>
+        <div class=" banner container-fluid d-flex align-items-center justify-content-center">
+            <h1 class="text-white">Ilha das rabanadas
+                <p>Primeira loja especializada em produção de rabanadas</p>
+            </h1>
+        </div>
+    </section>
+    <section style="background-color:#FFBD59;">
+        <div class="container  p-3">
+            <h3 class="text-center">Cardápio</h3>
+            <h5 class="text-end">Visualizar cardápio completo</h5>
+            <div class="row justify-content-evenly align-items-center ">
+                <div class="col-4">
+                    <div class="card">
+                        <img src="<?php echo IMG ?>/img/rabanada-vegana.webp" alt="" class="fluid">
+                        <div class="card-body">
+                            <h3 class="card-title">Title</h3>
+                            <p class="card-text">Rabanads veganas</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-4">
+                    <div class="card">
+                        <img src="<?php echo IMG ?>/img/rabanada-doce.webp" alt="" class="img-fluid w-100">
+                        <div class="card-body">
+                            <h3 class="card-title">Title</h3>
+                            <p class="card-text">Rabanadas Doces</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-4">
+                    <div class="card">
+                        <img src="<?php echo IMG ?>/img/rabanada-salgada.webp" alt="" class="fluid">
+                        <div class="card-body">
+                            <h3 class="card-title">Title</h3>
+                            <p class="card-text">Rabanadas Salgadas</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="x" class="">
+
+        <div class="container p-3">
+            <h3 class="text-center">
+                Serviços prestados
+            </h3>
+
+            <div class="container">
+                <div class="row justify-content-center align-items-center g-2">
+                    <div class="col">
+                        <ul>
+                            <li>Venda a varejo de produtos (Loja fisíca).</li>
+                            <li> Venda de serviços para buffet </li>
+                            <li>Serviço de delivery própio</li>
+                            <li>Produtos personalizados</li>
+                            <li>Alimentos fitness</li>
+                        </ul>
+                    </div>
+                    <div class="col d-flex justify-content-center"><img width="70%" src="<?php echo IMG ?>/img/servico-prestado.webp" alt=""></div>
+                </div>
+            </div>
+
+        </div>
+
+
+
+
+
+    </section>
+>>>>>>> Stashed changes
 
     <!-- Bootstrap JavaScript Libraries -->
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">

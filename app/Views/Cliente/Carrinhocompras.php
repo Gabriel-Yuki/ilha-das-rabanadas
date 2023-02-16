089e51f29f45ca9eb0fc116333136acf6c85db86
Jonathan  Guimarães <guimaraesjonathan92@gmail.com>
2023-02-16 10:26:26 -0300

    carrinho de compras

--- app/Views/Cliente/Carrinhocompras.php
+++ app/Views/Cliente/Carrinhocompras.php
@@ -6,72 +6,93 @@ include "./app/config.php"; ?>
 <head>
     <title>Title</title>
     <!-- Required meta tags -->
+
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <style>
-        h1 {
-            text-align: center;
 
-        }
-
-        button.btn.btn-primary.btn-sm {
-            background-color: rgba(255, 189, 89, 0.8);
-        }
-
-        button.btn.btn-secondary.btn-sm {
-            background-color: rgba(65, 191, 97, 1);
-        }
-    </style>
+    <script src="https://cdn.lordicon.com/ritcuqlt.js"></script>
     <!-- Bootstrap CSS v5.2.1 -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
 
     <link rel="stylesheet" href="<?php echo CSS_PADRAO; ?>">
+    <style>
+        section {
+            background: aqua;
+            width: 70%!important;
+            height: 62vh;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+        }
+        .card-content{
 
+            max-width:90%;
+        }
+
+.col{
+    max-height: 100%;
+    max-width: 30%;
+}
+        input[type=number] {
+            width: 25% !important;
+            display: inline;
+
+        }
+    </style>
 </head>
 
 <body>
     <!-- Bootstrap JavaScript Libraries -->
+    <?php
+
+    include './app/Views/Headers/headerManager.php';
+    ?>
+
+    <main>
 
 
-    <h1>Carrinho de Compras</h1>
 
-    <center>
-        <div class="container align-items-center">
-            <div class="card mb-3" style="max-width: 540px;">
+
+        <section class="container ">
+            <div class= "card card-content  mb-3">
                 <div class="row g-0">
-                    <div class="col-md-4">
-                        <img src="###" class="img-fluid rounded-start" alt="...">
+                    <div class="col">
+                        <img src="<?php echo IMG ?>/img/rabanada-salgada.webp " class="" alt="..." height="100%" width="90%">
                     </div>
-                    <div class="col-md-8">
+                    <div class="col">
                         <div class="card-body">
-                            <h5 class="card-title">Rabanada Vegana</h5>
-                            <p class="card-text">Valor R$ 5,00</p>
-                            <p class="card-text">Quantidade: </p>
-                            <p class="card-text">Total: </p>
+                            <h5 class="card-title">Rabanda com brigadeiro</h5>
+                            <p class="card-text">Valor:R$ <span>400</span></p>
+
+                            <div class="mb-3">
+                                <label for="" class="form-label">
+                                    <span>Quantidade</span>
+
+
+                                    <input type="number" name="" id="quantidade" class="form-control" placeholder="" aria-describedby="helpId">
+                                </label>
+
+
+
+                            </div>
+
+                            <p>Total: <span>2000</span></p>
                         </div>
                     </div>
                 </div>
             </div>
+        </section>
+    </main>
 
-            <div class="card mb-3" style="max-width: 540px;">
-                <div class="row g-0">
-                    <div class="col-md-8">
-                        <div class="card-body">
-                            <h5 class="card-title">Endereço de Entrega</h5>
-                            <p class="card-text">Rua: <input type="text"></p>
-                            <p class="card-text">Data da entrega: <input type="date"></p>
-                        </div>
-                    </div>
-                </div>
-            </div>
 
-        </div>
 
-        <button type="button" class="btn btn-primary btn-sm">limpar carrinho</button>
+    <button type="button" class="btn btn-primary btn-sm">limpar carrinho</button>
 
-        <button type="button" class="btn btn-secondary btn-sm">Fechar compras</button>
-    </center>
+    <button type="button" class="btn btn-secondary btn-sm">Fechar compras</button>
 
+
+    <?php
+    include FOOTER; ?>
 
 
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
